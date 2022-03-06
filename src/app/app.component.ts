9fc619e7b1e8ddfb37796f4575866ad3524a7a3b
Luciano de Sousa Lopes <lucianodesousalopes.github@gmail.com>
2022-03-05 21:50:24 -0300

    Project started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'working-with-pipes';
+  name: string = '';
+  amount: number = 0;
+  stringDate: string = '';
+  height: number = 0;
+
+  onNameChange(event: Event) {
+    this.name = (event.target as HTMLInputElement).value;
+  }
+
+  onAmountChange(event: Event) {
+    this.amount = Number((event.target as HTMLInputElement).value);
+  }
+
+  onDateChange(event: Event) {
+    this.stringDate = (event.target as HTMLInputElement).value;
+  }
+
+  onHeightChange(event: Event) {
+    this.height = Number((event.target as HTMLInputElement).value);
+  }
 }
